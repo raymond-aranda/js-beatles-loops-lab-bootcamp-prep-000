89ba8f2a2f7031b367d977db20a1c943c5481583
Raymond Aranda <raymond.aranda@gmail.com>
2017-09-13 06:37:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function theBeatlesPlay(musicians, instruments) {
+  var arr = []
+
+  for (var i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return arr
+}
+
+function johnLennonFacts(facts) {
+
+  var factsArr = []
+
+  var i = 0
+  while(i < facts.length) {
+    factsArr.push(`${facts}!!!`)
+  }
+}
